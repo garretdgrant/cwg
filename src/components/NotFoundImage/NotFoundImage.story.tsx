e43a6d95423ddf3bd370ce29414b8e9454be3dd3
Garret Grant <garretgrant@Garrets-MacBook-Pro.local>
2024-11-30 14:51:05 -0800

    chore: add node types

--- src/components/NotFoundImage/NotFoundImage.story.tsx
+++
@@ -1,9 +0,0 @@
-import { StoryWrapper } from '../../components/StoryWrapper/StoryWrapper';
-import attributes from './attributes.json';
-import { NotFoundImage } from './NotFoundImage';
-
-export default { title: 'NotFoundImage' };
-
-export function Usage() {
-  return <StoryWrapper attributes={attributes} component={NotFoundImage} />;
-}
