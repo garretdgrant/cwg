9ef09d2afdad09603de7f558dd646aeb5d793a61
Garret Grant <garretgrant@Garrets-MacBook-Pro.local>
2024-11-30 15:08:55 -0800

    chore: update vite with _redirects

--- src/components/NotFoundImage/NotFoundImage.tsx
+++ src/components/NotFoundImage/NotFoundImage.tsx
@@ -1,8 +1,10 @@
 import { Button, Container, Image, SimpleGrid, Text, Title } from '@mantine/core';
 import image from './image.svg';
 import classes from './NotFoundImage.module.css';
+import { useNavigate } from 'react-router-dom';
 
 export function NotFoundImage() {
+  const navigate = useNavigate();
   return (
     <Container className={classes.root}>
       <SimpleGrid spacing={{ base: 40, sm: 80 }} cols={{ base: 1, sm: 2 }}>
@@ -13,7 +15,7 @@ export function NotFoundImage() {
             Page you are trying to open does not exist. You may have mistyped the address, or the
             page has been moved to another URL. If you think this is an error contact support.
           </Text>
-          <Button variant="outline" size="md" mt="xl" className={classes.control}>
+          <Button onClick={() => navigate('/')} variant="outline" size="md" mt="xl" className={classes.control}>
             Get back to home page
           </Button>
         </div>
--- vite.config.mjs
+++ vite.config.mjs
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import {viteStaticCopy} from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), viteStaticCopy({
+    targets: [{
+      src: '_redirects',
+      dest: '.'
+    }]
+  })],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './vitest.setup.mjs',
   },
+  
 });
