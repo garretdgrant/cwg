34597398e08a40b0d7e5dc80ff7475a788c7ce2b
Garret Diego Grant <103865764+garretdgrant@users.noreply.github.com>
2024-12-07 00:14:03 -0800

    feat: contact form validation (#9)
    * feat: form validation
    
    * chore: update form
    
    * chore: lint and change test to ci
    
    ---------
    
    Co-authored-by: Garret Grant <garretgrant@Garrets-MacBook-Pro.local>

--- src/components/GetInTouch/GetInTouch.tsx
+++ src/components/GetInTouch/GetInTouch.tsx
@@ -3,17 +3,32 @@ import {
   Container,
   Group,
   Image,
+  NativeSelect,
   Paper,
   SimpleGrid,
   Text,
   Textarea,
   TextInput,
 } from '@mantine/core';
+import { useForm } from '@mantine/form';
 import { ContactIconsList } from './ContactIcons';
 import pattern from './polygon.svg';
 import classes from './GetInTouch.module.css';
 
 export function GetInTouch() {
+  const form = useForm({
+    mode: 'uncontrolled',
+    initialValues: { name: '', email: '', subject: '', message: '' },
+    validate: {
+      name: (value: string) => (value.length < 3 ? 'Name must have at least 3 letters' : null),
+      email: (value: string) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+      subject: (value: string) =>
+        value.length < 3 ? 'Subject must have at least 3 letters' : null,
+      message: (value: string) =>
+        value.length < 25 ? 'Message must have at least 3 letters' : null,
+    },
+  });
+
   return (
     <Container className={classes.outter}>
       <Image className={classes.pattern} src={pattern} pos="absolute" w="100%" />
@@ -32,8 +47,9 @@ export function GetInTouch() {
             name="contact" // Must match the hidden form's name
             method="POST"
             data-netlify="true" // Enables Netlify Forms
-            netlify-honeypot="bot-field" // Adds a honeypot field for spam protection
-            action="/contact" // Redirect after successful submission
+            netlify-honeypot="bot-field" // Optional: Adds a honeypot field for spam protection
+            onSubmit={form.onSubmit(form.validate)}
+
           >
             {/* Hidden input for form name */}
             <input type="hidden" name="form-name" value="contact" />
@@ -56,14 +72,44 @@ export function GetInTouch() {
                   placeholder="Your name"
                   name="name" // Must match hidden form
                   required
+                  key={form.key('name')}
+                  {...form.getInputProps('name')}
                 />
                 <TextInput
                   label="Your email"
                   placeholder="hello@mantine.dev"
                   name="email" // Must match hidden form
                   required
+                  key={form.key('email')}
+                  {...form.getInputProps('email')}
                 />
               </SimpleGrid>
+
+              <NativeSelect
+                mt="md"
+                name="hear"
+                label="How did you hear about us?"
+                key={form.key('hear')}
+                {...form.getInputProps('here')}
+                data={[
+                  {
+                    group: 'Online',
+                    items: [
+                      { label: 'other', value: 'other' },
+                      { label: 'Instagram', value: 'instagram' },
+                      { label: 'Facebook', value: 'facebook' },
+                      { label: 'Web Search (google, bing, etc)', value: 'web' },
+                    ],
+                  },
+                  {
+                    group: 'Referral',
+                    items: [
+                      { label: 'Referral', value: 'referral' },
+                      { label: 'other', value: 'other' },
+                    ],
+                  },
+                ]}
+              />
 
               <TextInput
                 mt="md"
@@ -71,6 +117,8 @@ export function GetInTouch() {
                 placeholder="Subject"
                 name="subject" // Must match hidden form
                 required
+                key={form.key('subject')}
+                {...form.getInputProps('subject')}
               />
 
               <Textarea
@@ -80,6 +128,8 @@ export function GetInTouch() {
                 minRows={3}
                 name="message" // Must match hidden form
                 required
+                key={form.key('message')}
+                {...form.getInputProps('message')}
               />
 
               {/* reCAPTCHA Widget (Optional) */}
