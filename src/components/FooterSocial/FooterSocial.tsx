6953235b3134631802b7e32f0c3680c4adaa2cb2
Garret Grant <garretgrant@Garrets-MacBook-Pro.local>
2024-12-07 13:02:37 -0800

    chore: update socials on footer

--- src/components/FooterSocial/FooterSocial.tsx
+++ src/components/FooterSocial/FooterSocial.tsx
@@ -1,4 +1,4 @@
-import { IconBrandInstagram, IconBrandTwitter, IconBrandYoutube } from '@tabler/icons-react';
+import { IconBrandInstagram, IconBrandLinkedin, IconBrandMeta } from '@tabler/icons-react';
 import { ActionIcon, Container, Group, Image, Text } from '@mantine/core';
 import logo from '@/favicon.svg';
 import { useNavigateToTop } from '@/hooks/useNavigateToTop';
@@ -6,6 +6,15 @@ import classes from './FooterSocial.module.css';
 
 export function FooterSocial() {
   const navigate = useNavigateToTop();
+  const socials = {
+    linkedIn: 'https://www.linkedin.com/in/garret-d-grant/',
+    instagram: 'https://www.instagram.com/garret.grant/',
+    facebook: 'https://www.facebook.com/garret.d.grant',
+  }
+  const handleClick = (link: string) => {
+    window.open(link,  '_blank', 'noopener,noreferrer')
+  }
+
   return (
     <div className={classes.footer}>
       <Container className={classes.inner}>
@@ -16,14 +25,14 @@ export function FooterSocial() {
           <Text>El Dorado Web Design</Text>
         </Group>
         <Group gap={0} className={classes.links} justify="flex-end" wrap="nowrap">
-          <ActionIcon size="lg" color="gray" variant="subtle">
-            <IconBrandTwitter size={18} stroke={1.5} />
+          <ActionIcon onClick={()=>handleClick(socials.linkedIn)} size="xl" color="gray" variant="subtle">
+            <IconBrandLinkedin size={28} stroke={1.25} />
           </ActionIcon>
-          <ActionIcon size="lg" color="gray" variant="subtle">
-            <IconBrandYoutube size={18} stroke={1.5} />
+          <ActionIcon onClick={()=>handleClick(socials.facebook)} size="xl" color="gray" variant="subtle">
+            <IconBrandMeta size={28} stroke={1.25} />
           </ActionIcon>
-          <ActionIcon size="lg" color="gray" variant="subtle">
-            <IconBrandInstagram size={18} stroke={1.5} />
+          <ActionIcon onClick={()=>handleClick(socials.instagram)} size="xl" color="gray" variant="subtle">
+            <IconBrandInstagram size={28} stroke={1.25} />
           </ActionIcon>
         </Group>
       </Container>
